bdea7f264d1b92b332d2a5970cc5eab74cd07bcc
Kyle Hackney <kyle.m.hackney@gmail.com>
2024-08-05 10:09:43 -0500

    calculates the total of a range of even numbers

---
+++ for-loops3.py
@@ -0,0 +1,9 @@
+target =int(input("enter a number")) # Enter a number between 0 and 1000
+# 🚨 Do not change the code above ☝️
+
+# Write your code here 👇
+sneep= 0
+for target in range(0, target+1 ,2):
+  sneep += target
+  
+print(sneep)
