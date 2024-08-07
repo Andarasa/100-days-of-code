9815a0afcf97b45fac218a3bc6f1c20c1ba2c6d1
Kyle Hackney <kyle.m.hackney@gmail.com>
2024-08-07 18:50:40 -0500

    solves reeborg maze including edge cases

---
+++ Reeborgmaze.py
@@ -0,0 +1,21 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def newall():
+        if wall_on_right() and wall_in_front()== True:
+            turn_left()
+        elif front_is_clear() and right_is_clear() == True:
+            move()
+        elif right_is_clear() == True:
+            turn_right()
+            move()
+        elif wall_on_right() == True:
+            move()
+        
+        else:
+            move()
+while at_goal() != True:
+    newall()   
+else:
+    pause()
