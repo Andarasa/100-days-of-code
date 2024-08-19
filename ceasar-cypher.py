dccf39473b2cefec889fa2e2474ea5769960ca95
Kyle Hackney <kyle.m.hackney@gmail.com>
2024-08-18 23:00:42 -0500

    added functionality for spaces and special chars

--- ceasar-cypher.py
+++ ceasar-cypher.py
@@ -10,13 +10,30 @@ def ceasar(original_text, shift_amount, encode_or_decode):
     cypher_text = ""
 
     for letter in original_text:
-        if direction == "decode":
-            shifted_position = alphabet.index(letter) - shift_amount
-        elif direction == "encode":
-            shifted_position = alphabet.index(letter) + shift_amount
-        shifted_position  %= len(alphabet)
-        cypher_text += alphabet[shifted_position]
+        if letter not in alphabet:
+            cypher_text += letter
+        else:
+            if direction == "decode":
+                shifted_position = alphabet.index(letter) - shift_amount
+            elif direction == "encode":
+                shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position  %= len(alphabet)
+            cypher_text += alphabet[shifted_position]
     print(f"Here is your decoded result: {cypher_text}")
 
-ceasar(original_text=text, shift_amount=letter_shift, encode_or_decode=direction)
 
+should_continue = True
+
+while should_continue:
+
+    direction = input("Type encode to encrypt, type decode to decrypt\n").lower()
+    text = input("What message do you want to encode?\n").lower()
+    letter_shift = int(input("How many letters do you want to shift by?\n"))
+
+    ceasar(original_text=text, shift_amount=letter_shift, encode_or_decode=direction)
+
+    final=  input("Do you want to keep going? Type yes or no.\n")
+
+    if final == "no":
+        should_continue = False
+        print("thanks for playing!")
